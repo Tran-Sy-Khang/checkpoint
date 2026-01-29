8654ba82cfdc4a471c7fc898e21d90cc1f30851c
Tran Sy Khang <khang.transy2504@gmail.com>
2026-01-29 19:07:13 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,265 @@
+let San_pham = [
+  // Bộ quần áo
+  // 1
+  {
+    id: 1,
+    Name: "Quần áo bảo hộ chống hóa chất dùng 1 lần Spacel C",
+    image: "./San_pham1.png",
+    price: "500.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //2
+  {
+    id: 2,
+    Name: "Bộ quần áo cản điện trường",
+    image: "./san_pham2.jpg",
+    price: "600.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //3
+  {
+    id: 3,
+    Name: " Bộ quần áo chống hóa chất ALPHATEC 3000, model 111",
+    image: "./san_pham3.jpg",
+    price: "650.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //4
+  {
+    id: 4,
+    Name: "Quần áo chống hóa chất LakeLand CT3S428",
+    image: "./san_pham4.jpg",
+    price: "550.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //5
+  {
+    id: 5,
+    Name: "Quần áo BHLĐ chống hóa chất model MC4000",
+    image: "./san_pham5.jpg",
+    price: "650.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //6
+  {
+    id: 6,
+    Name: "Bộ quần áo mưa Hàn Quốc SI-911",
+    image: "./san_pham6.jpg",
+    price: "700.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //   7
+  {
+    id: 7,
+    Name: "Quần quần áo chịu nhiệt lạnh Nitơ -250c (6 món)",
+    image: "./san_pham7.jpg",
+    price: "630.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //   8
+  {
+    id: 8,
+    Name: "Bộ quần áo tráng bạc KTFS500 Korea",
+    image: "./san_pham8.jpg",
+    price: "720.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //   9
+  {
+    id: 9,
+    Name: "Bộ quần áo chống cháy Nomex CHINA theo thông tư 56",
+    image: "./san_pham9.jpg",
+    price: "780.000₫",
+    catagory: "Bộ quần áo",
+  },
+  //   10
+  {
+    id: 10,
+    Name: "Quần áo bảo hộ vải kaki ngắn tay",
+    image: "./san_pham10.jpg",
+    price: "580.000₫",
+    catagory: "Bộ quần áo",
+  },
+
+  //   Mũ
+  //   11
+  {
+    id: 11,
+    Name: "Mũ bảo hộ lao động North A59R Vàng",
+    image: "./san_pham11.jpg",
+    price: "300.000₫",
+    catagory: "Mũ",
+  },
+  //12
+  {
+    id: 12,
+    Name: "Mũ bảo hộ COV có kính bảo vệ",
+    image: "./san_pham12.jpg",
+    price: "350.000₫",
+    catagory: "Mũ",
+  },
+  //   13
+  {
+    id: 13,
+    Name: "Mũ chống hồ quang điện AS1200HAT-PP 12 cal/cm2",
+    image: "./san_pham13.jpg",
+    price: "400.000₫",
+    catagory: "Mũ",
+  },
+  //14
+  {
+    id: 14,
+    Name: "MŨ BẢO HỘ CÓ LỖI THÔNG KHÍ COVH-A001",
+    image: "./san_pham14.jpg",
+    price: "350.000₫",
+    catagory: "Mũ",
+  },
+  //15
+  {
+    id: 15,
+    Name: "Mũ bảo hộ 3M H702R",
+    image: "./san_pham15.jpg",
+    price: "420.000₫",
+    catagory: "Mũ",
+  },
+  //16
+  {
+    id: 16,
+    Name: "Mũ tai bèo chống nắng KTOM",
+    image: "./san_pham16.jpg",
+    price: "300.000₫",
+    catagory: "Mũ",
+  },
+  //17
+  {
+    id: 17,
+    Name: "Mũ trùm đầu vải nhôm chống cháy Otego 8007",
+    image: "./san_pham17.jpg",
+    price: "320.000₫",
+    catagory: "Mũ",
+  },
+  //18
+  {
+    id: 18,
+    Name: "Mũ chống cháy KTFH500 Korea",
+    image: "./san_pham18.jpg",
+    price: "370.000₫",
+    catagory: "Mũ",
+  },
+  //19
+  {
+    id: 19,
+    Name: "Chụp tai chống ồn gắn mũ bảo hộ L2 Muff",
+    image: "./san_pham19.jpg",
+    price: "470.000₫",
+    catagory: "Mũ",
+  },
+  //20
+  {
+    id: 20,
+    Name: "Mũ trùm đầu chịu nhiệt 1600 độ C 4HV",
+    image: "./san_pham20.jpg",
+    price: "420.000₫",
+    catagory: "Mũ",
+  },
+  //21
+  {
+    id: 21,
+    Name: "Kính bảo hộ A902 đen Sperian",
+    image: "./san_pham21.jpg",
+    price: "100.000₫",
+    catagory: "Kính",
+  },
+  //22
+  {
+    id: 22,
+    Name: "Kính bảo hộ lao động Kings màu đen KY2224",
+    image: "./san_pham22.jpg",
+    price: "110.000₫",
+    catagory: "Kính",
+  },
+  //23
+  {
+    id: 23,
+    Name: "Kính chống hóa chất V-Maxx 1006194",
+    image: "./San_pham23.jpg",
+    price: "120.000₫",
+    catagory: "Kính",
+  },
+  //24
+  {
+    id: 24,
+    Name: "Kính bảo hộ A700 Honeywell trong suốt 1015361",
+    image: "./san_pham24.jpg",
+    price: "130.000₫",
+    catagory: "Kính",
+  },
+  //25
+  {
+    id: 25,
+    Name: "Kính chống hóa chất DuraMaxx 1017751",
+    image: "./san_pham25.jpg",
+    price: "140.000₫",
+    catagory: "Kính",
+  },
+  //26
+];
+
+console.log(San_pham);
+
+// Sử dụng hàm include để search
+
+// const search = document.getElementById("Search");
+
+// function removeVietnameseTones(str) {
+//   return (
+//     str
+//       // .normalize("NFD")
+//       .replace(/[\u0300-\u036f]/g, "")
+//       .replace(/đ/g, "d")
+//       .replace(/Đ/g, "D")
+//       .toLowerCase()
+//   );
+// }
+
+// function searchProducts(search) {
+//   const normalizedKeyword = removeVietnameseTones(search);
+//   return San_pham.filter((product) => {
+//     return removeVietnameseTones(product.name).includes(normalizedKeyword);
+//   });
+// }
+
+document.getElementById("btn").addEventListener("click", function () {
+  const search = document.getElementById("search").value;
+
+  console.log(search);
+});
+
+function searchProducts(search) {
+  const lowerKeyword = search.toLowerCase();
+  return San_pham.filter((product) => {
+    return product.Name.toLowerCase().includes(lowerKeyword);
+  });
+}
+
+console.log(searchProducts(search));
+const Ket_qua = searchProducts(search).length;
+console.log(Ket_qua);
+
+for (let i = 0; i < searchProducts(search).length; i++) {
+  document.getElementById("Cac_san_pham").innerHTML += `
+    <div class="product">
+      <img src="${
+        searchProducts(search)[i].image
+      }" alt="không có kết nối" style="width: 268.9px; height: 268.9px">
+      <p>${searchProducts(search)[i].Name}</p>
+      <p>Giá tiền: ${searchProducts(search)[i].price}</p>
+      <button id="${searchProducts(search)[i].id}"> thêm vào giở hàng </button>
+    </div>
+  `;
+}
+
+document.getElementById(
+  "So_san_pham"
+).innerHTML = `Số sản phẩm tìm thấy: ${Ket_qua}`;
+console.log(p.innerText);

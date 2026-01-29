d49414890a3cbd4eb97fde3b2c049872be6b00f0
Tran Sy Khang <khang.transy2504@gmail.com>
2026-01-29 20:09:53 +0700

    commit

--- script.js
+++ script.js
@@ -229,37 +229,52 @@ console.log(San_pham);
 //   });
 // }
 
+const login = false;
+
 document.getElementById("btn").addEventListener("click", function () {
   const search = document.getElementById("search").value;
 
-  console.log(search);
-});
+  function searchProducts(search) {
+    const lowerKeyword = search.toLowerCase();
+    return San_pham.filter((product) => {
+      return product.Name.toLowerCase().includes(lowerKeyword);
+    });
+  }
 
-function searchProducts(search) {
-  const lowerKeyword = search.toLowerCase();
-  return San_pham.filter((product) => {
-    return product.Name.toLowerCase().includes(lowerKeyword);
-  });
-}
+  console.log(searchProducts(search));
+  const Ket_qua = searchProducts(search).length;
+  console.log(Ket_qua);
 
-console.log(searchProducts(search));
-const Ket_qua = searchProducts(search).length;
-console.log(Ket_qua);
-
-for (let i = 0; i < searchProducts(search).length; i++) {
-  document.getElementById("Cac_san_pham").innerHTML += `
+  document.getElementById("Cac_san_pham").innerHTML = "";
+  for (let i = 0; i < searchProducts(search).length; i++) {
+    document.getElementById("Cac_san_pham").innerHTML += `
     <div class="product">
       <img src="${
         searchProducts(search)[i].image
       }" alt="không có kết nối" style="width: 268.9px; height: 268.9px">
       <p>${searchProducts(search)[i].Name}</p>
       <p>Giá tiền: ${searchProducts(search)[i].price}</p>
-      <button id="${searchProducts(search)[i].id}"> thêm vào giở hàng </button>
+      <button id="${
+        searchProducts(search)[i].id
+      }" class="Buy" type="Submit"> thêm vào giở hàng </button>
     </div>
   `;
-}
+  }
 
-document.getElementById(
-  "So_san_pham"
-).innerHTML = `Số sản phẩm tìm thấy: ${Ket_qua}`;
-console.log(p.innerText);
+  document.getElementById(
+    "So_san_pham"
+  ).innerHTML = `Số sản phẩm tìm thấy: ${Ket_qua}`;
+  console.log(p.innerText);
+
+  window.location.href = "Search.html";
+
+  console.log(search);
+});
+
+document.getElementsByClassName("Buy").addEventListener("click", function () {
+  if ((login = false)) {
+    window.location.href;
+  }
+});
+
+// sign in - sign up
